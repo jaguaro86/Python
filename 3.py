1e6f5343104922c21c6b527bcdde6d500a82218a
jaguaro86 <43296989+jaguaro86@users.noreply.github.com>
2021-02-10 21:23:26 +0700

    Попробовал без конкатенации)

--- 3.py
+++ 3.py
@@ -1,7 +1,4 @@
-user_input = str(input("Введите число: "))
+ui = int(input("Введите число: "))
 
-num1 = int(user_input)
-num2 = int(user_input+user_input)
-num3 = int(user_input+user_input+user_input)
-
-print(num1+num2+num3)
+num = ui + int(f"{ui}{ui}") + int(f"{ui}{ui}{ui}")
+print(num)
